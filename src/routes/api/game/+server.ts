59e2f209435b2da29986dc7787181551daca2648
Tawab Safi <asaficontact@gmail.com>
2024-07-21 22:44:24 -0400

    comment out some consoles

--- src/routes/api/game/+server.ts
+++ src/routes/api/game/+server.ts
@@ -2,6 +2,10 @@ import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
 import { wordList } from '$lib/stores';
 
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+
+
+
 
 // In-memory storage for game sessions (replace with a database in production)
 let gameSessions: Record<string, GameSession> = {};
@@ -24,8 +28,7 @@ function initializeWordDict(): Record<string, number> {
 
 export const POST: RequestHandler = async ({ request }) => {
     const { playerId } = await request.json();
-    console.log('The following playerId was received:', playerId);
-
+    //console.log('The following playerId was received:', playerId);
 
     // Create a new game session
     const newSession: GameSession = {
@@ -36,7 +39,7 @@ export const POST: RequestHandler = async ({ request }) => {
         incorrectWords: initializeWordDict()
     };
 
-    console.log('The following newSession was created:', newSession);
+    //console.log('The following newSession was created:', newSession);
 
     gameSessions[playerId] = newSession;
 
@@ -47,11 +50,11 @@ export const POST: RequestHandler = async ({ request }) => {
 export const PUT: RequestHandler = async ({ request }) => {
     const { playerId, score, additionalMeatCount, word, isCorrect } = await request.json();
 
-    console.log('The following playerId was received:', playerId);
-    console.log('The following score was received:', score);
-    console.log('The following meatCount was received:', additionalMeatCount);
-    console.log('The following word was received:', word);
-    console.log('The following isCorrect was received:', isCorrect);
+    //console.log('The following playerId was received:', playerId);
+    //console.log('The following score was received:', score);
+    //console.log('The following meatCount was received:', additionalMeatCount);
+    //console.log('The following word was received:', word);
+    //console.log('The following isCorrect was received:', isCorrect);
 
     if (!gameSessions[playerId]) {
         return json({ error: 'Game session not found' }, { status: 404 });
@@ -61,23 +64,22 @@ export const PUT: RequestHandler = async ({ request }) => {
     const session = gameSessions[playerId];
     session.score += score;
     session.totalMeatCount += additionalMeatCount;
-    console.log('The following session.score was updated:', session.score);
-    console.log('The following session.totalMeatCount was updated:', session.totalMeatCount)
+    //console.log('The following session.score was updated:', session.score);
+    //console.log('The following session.totalMeatCount was updated:', session.totalMeatCount)
     if (isCorrect) {
         if (session.correctWords[word] || session.correctWords[word] === 0) {
             session.correctWords[word] = (session.correctWords[word] || 0) + 1;
         };
-        console.log('The following session.correctWords[word] was updated:', word);
-        console.log('The following session.correctWords[word] count:', session.correctWords[word]);
+        //console.log('The following session.correctWords[word] was updated:', word);
+        //console.log('The following session.correctWords[word] count:', session.correctWords[word]);
     } else {
         if (session.incorrectWords[word] || session.incorrectWords[word] === 0) {
             session.incorrectWords[word] = (session.incorrectWords[word] || 0) + 1;
         };
-        console.log('The following session.incorrectWords[word] was updated:', word);
-        console.log('The following session.incorrectWords[word] count:', session.incorrectWords[word]);
+        //console.log('The following session.incorrectWords[word] was updated:', word);
+        //console.log('The following session.incorrectWords[word] count:', session.incorrectWords[word]);
     }
 
-    console.log('The correct words are:', session.correctWords);
 
     return json({ message: 'Game session updated', session });
 };
