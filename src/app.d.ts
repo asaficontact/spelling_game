a5a006e8882ceea59d216bfb5a2978b16cc43319
Tawab Safi <asaficontact@gmail.com>
2024-07-26 14:14:52 -0400

    Added Server-side supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,19 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-import type { SupabaseClient } from '@supabase/supabase-js';
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js'
 declare global {
 	namespace App {
-		interface Locals {
-			supabase: SupabaseClient;
-		}
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			supabase: SupabaseClient<Database>;
+			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
+			session: Session | null
+			user: User | null
+		}
+		interface PageData {
+			session: Session | null
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
 }
 
-export { };
+export { }
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,9 @@
+// src/routes/+layout.server.ts
 import type { LayoutServerLoad } from './$types'
 
-export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cookies }) => {
-    const { session, user } = await safeGetSession()
-
+export const load: LayoutServerLoad = async ({ locals: { session }, cookies }) => {
     return {
         session,
-        user,
         cookies: cookies.getAll(),
     }
 }
